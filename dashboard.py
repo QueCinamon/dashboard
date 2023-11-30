c9d41996d9ab27a9d5f91bd342b40523bba5c962
QueCinamon <61902841+QueCinamon@users.noreply.github.com>
2023-11-30 07:44:39 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,7 +20,7 @@ st.set_page_config( page_icon= ':bar_chart:', layout= 'wide',page_title= 'Stream
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html= True)
 
 # Load data
-data= 'C:\\Users\\ADMIN\\Desktop\\Que\\data\\Sample Superstore WB.xlsx'
+data= 
 df= pd.read_excel(data)
 
 # File upload
